5aac02abad31dd707de2559630f9851e94ecf43d
Fcatme <33803252+Fcatme@users.noreply.github.com>
2017-11-19 20:32:58 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -41,7 +41,7 @@ if [[ ${OS} == Ubuntu ]];then
 	echo "deb-src http://ppa.launchpad.net/webupd8team/java/ubuntu trusty main" >> /etc/apt/sources.list
 	apt-key adv --keyserver keyserver.ubuntu.com --recv-keys EEA14886
 	apt-get update
-	apt-get install oracle-java8-installer
+	apt-get install oracle-java8-installer -y
 	apt-get install git git-core curl build-essential openssl libssl-dev -y
 	git clone https://github.com/nodejs/node.git
     cd node
